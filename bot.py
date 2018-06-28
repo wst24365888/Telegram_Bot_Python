e8ad1053bc8b6af6ee337a3b110ccf352512fb9e
litttlechin <good.life.is.for.me@gmail.com>
2018-06-28 23:32:56 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
 import json
 import requests
-import time
-import urllib
-
-import config
-
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -22,26 +17,10 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates(offset=None):
+def get_updates():
     url = URL + "getUpdates"
-    if offset:
-        url += "?offset={}".format(offset)
     js = get_json_from_url(url)
     return js
-
-
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-
-
-def echo_all(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
 
 
 def get_last_chat_id_and_text(updates):
@@ -53,20 +32,10 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
-    text = urllib.parse.quote_plus(text)
+    text = "Hi"
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
 
-def main():
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
-        time.sleep(0.5)
-
-
-if __name__ == '__main__':
-    main()
+text, chat = get_last_chat_id_and_text(get_updates())
+send_message(text, chat)
