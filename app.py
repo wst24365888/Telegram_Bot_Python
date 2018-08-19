29fe1c13a4826eeb3330fe243ec00ac1d1e5dd4f
litttlechin <good.life.is.for.me@gmail.com>
2018-08-19 22:04:43 +0800

    up

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
 @server.route("/")
 def webhook():
     bot.remove_webhook()
