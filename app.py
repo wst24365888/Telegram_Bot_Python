9d49d2c2e712ceee9f9f3bf80d8354a109b82730
litttlechin <good.life.is.for.me@gmail.com>
2018-08-19 23:16:40 +0800

    up

--- app.py
+++ app.py
@@ -48,6 +48,39 @@ def weather():
 
     return reply
 
+
+def google(in):
+
+    reply = 'Google搜尋如下\n\n'
+
+    # Google 搜尋 URL
+    google_url = 'https://www.google.com.tw/search'
+
+    # 查詢參數
+    my_params = {'q': in}
+
+    # 下載 Google 搜尋結果
+    r = requests.get(google_url, params = my_params)
+
+    # 確認是否下載成功
+    if r.status_code == requests.codes.ok:
+        # 以 BeautifulSoup 解析 HTML 原始碼
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        # 觀察 HTML 原始碼
+        # print(soup.prettify())
+
+        # 以 CSS 的選擇器來抓取 Google 的搜尋結果
+        items = soup.select('div.g > h3.r > a[href^="/url"]')
+        for i in items:
+            # 標題
+            reply += "標題：" + i.text
+            # 網址
+            reply += "網址：" + i.get('href')
+
+    return reply
+
+
 def get_user_id(user_id):
 
     print(user_id)
