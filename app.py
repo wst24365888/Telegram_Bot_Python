c42b57d64b837d0841e236803019a7a6e38dc94e
litttlechin <good.life.is.for.me@gmail.com>
2018-08-20 16:24:54 +0800

    up

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ def get_user_id(user_id):
 def start(message):
     get_user_id(str(message.chat.id))
     print('command: /main_page')
-    print(str(message.user.id)+'\n'+message.user.username+'\n')
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name + '.\n這裡有部分功能可以嘗試><\n/weather')
 
 
@@ -63,7 +62,6 @@ def start(message):
 def get_weather(message):
     get_user_id(str(message.chat.id))
     print('command: /weather')
-    print(str(message.user.id)+'\n'+message.user.username+'\n')
     bot.reply_to(message, weather())
 
 
@@ -71,7 +69,6 @@ def get_weather(message):
 def echo_message(message):
     get_user_id(str(message.chat.id))
     print(message.text)
-    print(str(message.user.id)+'\n'+message.user.username+'\n')
     bot.reply_to(message, message.text)
 
 bot.polling()
