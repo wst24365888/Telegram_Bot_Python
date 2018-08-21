24b416541b524048400d719e3a4ce3f0e1681087
litttlechin <good.life.is.for.me@gmail.com>
2018-08-21 16:51:42 +0800

    up

--- app.py
+++ app.py
@@ -146,4 +146,19 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://littlechin-tg-python.herokuapp.com/' + TOKEN)
+    print('success')
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
