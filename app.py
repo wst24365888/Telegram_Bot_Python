840783d6be4a2eb132c2b5f41887403d9c28001f
litttlechin <good.life.is.for.me@gmail.com>
2018-08-23 21:32:02 +0800

    up

--- app.py
+++ app.py
@@ -125,6 +125,7 @@ def start(message):
     helpmess = '.\n這裡有部分功能可以嘗試><'
     helpmess += '\n/weather  - 查詢天氣小幫手'
     helpmess += '\n/tnfshnew - 查詢南一中新訊息'
+    helpmess += '\n/workornot - 查詢停班課資訊'
     bot.reply_to(message, 'Hi, ' + message.from_user.first_name + helpmess)
 
 
