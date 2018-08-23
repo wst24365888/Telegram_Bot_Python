f99a34a5e9dc724f2d883425fbd73d82ddcf189a
litttlechin <good.life.is.for.me@gmail.com>
2018-08-23 20:52:40 +0800

    up

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://tgbot-littlechin.herokuapp.com/callback' + TOKEN)
+    bot.set_webhook(url='https://tgbot-littlechin.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
